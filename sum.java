81070cab643c6cf4604449b3297097e6416a1d5e
mahisha1997 <42768422+mahisha1997@users.noreply.github.com>
2018-12-19 12:23:35 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class sum
+{
+public static void main(String[]args)
+{
+int i,sum=0,n;
+Scanner scan=new Scanner(System.in);
+n=scan.nextInt();
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println("sum");
+}
+}
